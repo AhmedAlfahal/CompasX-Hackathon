6d7eaa3ba361d1ea3d15ef468a07d57b62552663
Mohammed Ehsan <mehsankhan02@gmail.com>
2024-06-09 13:52:23 +0400

    home page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,18 +1,19 @@
+// src/components/LoginPage.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './LoginPage.css';
 
 const LoginPage = () => {
   return (
-    <div className="landing-page">
+    <div className="login-page">
       <header className="hero-section">
         <h1>Welcome to Compass Voting</h1>
         <div className="cta-buttons">
-          <Link to="/login" className="btn-primary">Login</Link>
+          <Link to="/login" className="btn-primary">Login With MetaMask</Link>
           <Link to="/streams" className="btn-secondary">Watch Streams</Link>
         </div>
       </header>
-      
+
       <footer className="footer">
         <div className="footer-links">
           <a href="#">Terms of Service</a>
@@ -20,9 +21,9 @@ const LoginPage = () => {
           <a href="#">Contact Us</a>
         </div>
         <div className="social-media">
-          <a href="#"><img src="facebook-icon.png" alt="Facebook"/></a>
-          <a href="#"><img src="twitter-icon.png" alt="Twitter"/></a>
-          <a href="#"><img src="instagram-icon.png" alt="Instagram"/></a>
+          <a href="#"><img src="facebook-icon.png" alt="Facebook" /></a>
+          <a href="#"><img src="twitter-icon.png" alt="Twitter" /></a>
+          <a href="#"><img src="instagram-icon.png" alt="Instagram" /></a>
         </div>
       </footer>
     </div>
